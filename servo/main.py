b1f5fb164e449abbdfcd5f3e4302c91887effd62
Sebastian King <sebastian-king@users.noreply.github.com>
2022-11-23 19:28:02 -0600

    Update main.py

--- servo/main.py
+++ servo/main.py
@@ -1,14 +1,55 @@
 import lib_para_360_servo as servoe
-import pigpio as pig
+import pigpio as pig_pio
 from time import sleep
+
+def get_angle(duty_cycle):
+    full_circle = 360
+    duty_cycle_min = 27
+    duty_cycle_max = 971
+
+    if not duty_cycle: # if the duty cycle hasn't been calibrated yet, the duty_cycle will be None
+        return 0
+
+    angle = (full_circle - 1) - ((duty_cycle - duty_cycle_min) * full_circle) / (duty_cycle_max - duty_cycle_min + 1)
+
+    angle = max(min((full_circle - 1), angle), 0)
+
+    return angle
+
+def calc_target_angle_clockwise(degrees, current_angle):
+    target_angle = current_angle + degrees
+    if target_angle > 360:
+        target_angle - 360;
+    return target_angle
+
+def calc_target_angle_anti_clockwise(degrees, current_angle):
+    target_angle = current_angle - degrees
+    if target_angle < 0:
+        360 + target_angle;
+    return target_angle
+
 if __name__ == '__main__':
-    pi = pig.pi()
+
+    pi = pig_pio.pi()
     read1 = servoe.read_pwm(pi=pi,gpio=23)
-    
-    # Based off of BCM numbering (so GPIO 12 which is 32 in board pin numbering). On a pi zero, 12 and 13 are the pwm pins
-    write1 = servoe.write_pwm(pi=pi,gpio=12)    
-    write1.set_speed(1) # I don't know if this is clockwise or counter-clockwise, but -1 goes the opposite direction. Also speeds are between -1 and 1
-    sleep(2)
-    write1.set_speed(-1)
-    sleep(2)
-    write1.stop()
+    write1 = servoe.write_pwm(pi=pi,gpio=12)
+    sleep(1)
+
+    original_angle = current_angle = get_angle(read1.read())
+    print("original angle", original_angle)
+
+    write1.set_speed(-0.2)
+
+    while True:
+        current_angle = get_angle(read1.read())
+        #print("measured angle", current_angle)
+        if current_angle > (original_angle + 60):
+            print("final angle", current_angle)
+            write1.stop()
+            break;
+
+    #write1.set_speed(-0.5)
+    #print("set speed negative")
+    #sleep(1)
+    #print("read", get_angle(read1.read()))
+    #write1.stop()
