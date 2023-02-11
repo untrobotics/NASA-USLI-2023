43a3f74c3228aec74ff02a61db7bfdf8c4d32d3c
Kenneth Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-02-11 14:40:41 -0600

    Create main.py

---
+++ MG90SCode/main.py
@@ -0,0 +1,28 @@
+import pigpio as pig
+
+# This is the servo for popping the payload out of the rocket
+
+class Servo:
+    def __init__(self, pi, gpio_pin):
+        self.pi = pi
+        self.gpio_pin = gpio_pin
+
+    def set_position(self, degrees: int):
+        self.pi.set_servo_pulsewidth(user_gpio=self.gpio_pin, pulsewidth=Servo.calc_pulsewidth(degrees))
+
+    @staticmethod
+    def calc_pulsewidth(degrees: int) -> float:
+        if degrees < 0:
+            degrees = 0
+        elif degrees > 180:
+            degrees = 180
+        return degrees / 180 * 2000 + 500
+
+
+if __name__ == '__main__':
+    pi = pig.pi()
+    gpio = 12
+    servo = Servo(pi, gpio)
+    servo.set_position(0)
+    while True:
+        servo.set_position(int(input("Enter position in degrees")))
