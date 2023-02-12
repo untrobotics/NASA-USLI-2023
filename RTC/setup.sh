31e72bc7448d44524e9118c5025a57d46b1e7a0f
Kenneth Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2023-02-11 18:58:27 -0600

    Fixed regex issues and smbus install
    python-smbus doesn't exist... python3-smbus instead
    Also escaped a '$'

--- RTC/setup.sh
+++ RTC/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo apt-get install python-smbus i2c-tools -y ;
+sudo apt-get install python3-smbus i2c-tools -y ;
 sudo echo 'dtoverlay=i2c-rtc,ds3231' >> /boot/config.txt ;
 sudo apt-get -y remove fake-hwclock
-sudo update-rc.d -f fake-hwclock remove -y
-sudo systemctl disable fake-hwclock -y
+sudo update-rc.d -f fake-hwclock remove
+sudo systemctl disable fake-hwclock
 sudo sed -ri 's/^(\s*if\s+\[\s+-e\s+\/run\/systemd\/system\s+\]\s+;\s+then.*fi\s*;?)(.*)/(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
 sudo sed -ri '/^(\s*if\s+\[\s+-e\s+\/run\/systemd\/system\s+\]\s+;\s+then.*)/,/^fi/s/(.*)/#\1/' /lib/udev/hwclock-set ;
-sudo sed -ri 's/(\/sbin\/hwclock --rtc=$dev --systz --badyear\s*;?)(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
-sudo sed -ri 's/(\/sbin\/hwclock --rtc=$dev --rtc=$dev --systz\s*;?)(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
+sudo sed -ri 's/(\/sbin\/hwclock --rtc=\$dev --systz --badyear?\s*;?)(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
+sudo sed -ri 's/(\/sbin\/hwclock --rtc=\$dev --systz?\s*;?)(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
+sudo sed -ri 's/(\/sbin\/hwclock --rtc=\$dev --hctosys\s*;?)(.*)/#\1\n\2/' /lib/udev/hwclock-set ;
