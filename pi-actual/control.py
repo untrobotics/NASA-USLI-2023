3b138e9d86df206dfa8a0bca65a605764139caeb
Sebastian King <sebastian.thomas.king@gmail.com>
2023-02-13 23:58:51 -0600

    add exception info

--- pi-actual/control.py
+++ pi-actual/control.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys
+import traceback
 
 from kiss import KISS
 
@@ -34,6 +36,21 @@ def main():
         return
     except Exception as e:
         print("Error occurred in main loop, re-running...", e)
+        # Get current system exception
+        ex_type, ex_value, ex_traceback = sys.exc_info()
+
+        # Extract unformatter stack traces as tuples
+        trace_back = traceback.extract_tb(ex_traceback)
+
+        # Format stacktrace
+        stack_trace = list()
+
+        for trace in trace_back:
+            stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (trace[0], trace[1], trace[2], trace[3]))
+
+        print("Exception type : %s " % ex_type.__name__)
+        print("Exception message : %s" % ex_value)
+        print("Stack trace : %s" % stack_trace)
         main()
 
 if __name__ == '__main__':
