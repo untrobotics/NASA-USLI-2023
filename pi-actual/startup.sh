9253f5d7b82f308dd6df46c2a0fba2d387823f68
Sebastian King <sebastian.thomas.king@gmail.com>
2023-03-25 23:56:10 -0500

    py3

--- pi-actual/startup.sh
+++ pi-actual/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #sudo service pigpiod start
 rtl_fm -f 144.39M - | direwolf -c /home/pi/NASA-USLI-2023/pi-actual/sdr.conf -r 24000 -D 1 - &
-python /home/pi/NASA-USLI-2023/pi-actual/control.py
+python3 /home/pi/NASA-USLI-2023/pi-actual/control.py
